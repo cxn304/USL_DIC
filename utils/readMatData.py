d12cb1e2c46d422975fbc56b5163093b6c90710b
cxn <799538436@qq.com>
2023-10-15 21:50:15 +0800

    .

--- utils/readMatData.py
+++ utils/readMatData.py
@@ -8,9 +8,6 @@ import scipy.io as sio
 
 class cxnDataset(Dataset):
     """
-     rfimage_files：rf图片存放地址根路径
-     dfimage_files：df图片存放地址根路径
-     u_files：unwrap存放地址根路径
      return: tuple: (x,y) x:[batch,2,imgsize,imgsize]
     """
     def __init__(self, train_root,isaugument, mask_root = None):
@@ -34,8 +31,6 @@ class cxnDataset(Dataset):
 
     def __getitem__(self, index):
 
-        # 如果不进行增强，直接读取图像数据并返回
-        # 这里的open_image是读取图像函数，可以用PIL、opencv等库进行读取
         rfimg = self.open_image(self.rfimage_files[index])
         dfimg = self.read_mat(self.dfimage_files[index])
         ulabel,elabel = self.read_label(self.ue_files[index])
@@ -48,10 +43,10 @@ class cxnDataset(Dataset):
         elabel = self.to_tensor(elabel) 
         img=torch.cat((rfimg,dfimg),0)
         if not self.mask_root:
-            return (img,ulabel,elabel)    # 返回tuple形式的训练集
+            return (img,ulabel,elabel)
         elif self.mask_root is not None:
             mask = self.to_tensor(self.open_image(self.mask_files[index]))
-            return (img,ulabel,elabel,mask)    # 返回tuple形式的训练集
+            return (img,ulabel,elabel,mask) 
             
     
     def read_mat(self,name):
@@ -68,7 +63,6 @@ class cxnDataset(Dataset):
         
     
     def augumentaion(self,rf,df,ul,el):
-        # 水平翻转
         p = np.random.choice([0, 1])
         if p:
             df=df[:,::-1]
@@ -85,38 +79,14 @@ class cxnDataset(Dataset):
         
     
     def to_tensor(self,img):
-        # unsqueeze(0)在第一维上增加一个维度
         img = torch.from_numpy(img.astype(np.float32)).unsqueeze(0)
         # img = img.repeat(3,1,1)
         return img
         
 
     def __len__(self):
-        # 返回图像的数量
         return len(self.rfimage_files)
     
-
-def haha():
-    a=cxnDataset('../train/mat训练/',isaugument=False)
-    for i, (xx, uu,ee) in enumerate(a):
-        ns = xx.size(1)
-        file_first_name = '%04d'%i
-        for noise_variance in np.arange(2,15,1):
-            noises = torch.randint(0, noise_variance, (1,ns,ns))
-            xy = noises+xx
-            rname = './train/'+'rf'+file_first_name+str(noise_variance)+'.png'
-            dname = './train/'+'df'+file_first_name+str(noise_variance)+'.png'
-            uename = './train/'+'ue'+file_first_name+str(noise_variance)+'.mat'
-            mdic = {"uu": uu.squeeze(0).numpy(), "e":ee.squeeze(0).numpy()}
-            # sio.savemat(uename, mdic)
-            # rf = Image.fromarray(xy[0,:,:].numpy().astype(np.uint8))
-            # rf.save(rname)
-            # df = Image.fromarray(xy[1,:,:].numpy().astype(np.uint8))
-            # df.save(dname)
-
-if __name__ == '__main__':
-    print(1)
-    #haha()  
 
 
 
